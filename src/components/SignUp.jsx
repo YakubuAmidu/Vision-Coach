a7be098a0d867733d1d1d32a1f9d1d7217038432
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-29 15:49:19 -0700

    Commiting changes

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -6,7 +6,10 @@ constructor(props) {
   super(props);
   this.state = {
     email: ' ',
-    password: ' '
+    password: ' ',
+    error: {
+      message: ' '
+    }
   }
 }
 
@@ -15,13 +18,13 @@ signUp() {
   const { email, password } = this.state;
   firebaseApp.auth().createUserWithEmailAndPassword(email, password)
   .catch(error => {
-    console.log('error', error);
+    this.setState({ error })
   })
 }
 
     render() {
         return (
-            <div className="form-inline">
+            <div className="form-inline" style={{ margin: '5%' }}>
             <h2>Sign Up</h2>
             <div className="form-group">
             <input
@@ -34,16 +37,19 @@ signUp() {
             className="form-control"
             type="password"
             placeholder="password"
+             style={{ marginRight: '5px' }}
             onChange={event => this.setState({ password: event.target.value })}
             />
             <button
             className="btn btn-primary"
+            style={{ marginRight: '5px' }}
             type="button"
             onClick={() => this.signUp()}
             >
             Sign Up
             </button>
             </div>
+            <div>{this.state.error.message}</div>
             </div>
         )
     }
