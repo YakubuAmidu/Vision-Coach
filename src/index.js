369b3b70a0d375c6c463db1b5785bddf56636ed0
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-29 14:15:21 -0700

    Commiting changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browswerHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Router path='/' history={browserHistory}>
+
+</Router>, document.getElementById('root'));
