7b6e3552970989393598838a0f632328c08d726a
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-29 15:05:37 -0700

    Commiting changes

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const config =  {
+  apiKey: "AIzaSyAeuaIjXz3N0UTOCaP3S3wtpJJ-oOmQi38",
+    authDomain: "visioncoach-f88e0.firebaseapp.com",
+    databaseURL: "https://visioncoach-f88e0.firebaseio.com",
+    projectId: "visioncoach-f88e0",
+    storageBucket: "visioncoach-f88e0.appspot.com",
+    messagingSenderId: "426136697856",
+    appId: "1:426136697856:web:c59b227bd434698124ece5",
+    measurementId: "G-HGHVCRY5M7"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { firebaseApp } from './firebase';
 
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+
+firebaseApp.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log('User has signed in or up', user);
+  } else {
+    console.log('User has signed out or still needs to signed out');
+  }
+})
 
 ReactDOM.render(
 <Router path="/" history={browserHistory}>
