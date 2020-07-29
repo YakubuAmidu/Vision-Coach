7995007ffe89258c26387bb56ac2ad2494cbe5c7
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-29 15:27:04 -0700

    Commiting changes

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -9,6 +9,10 @@ constructor(props) {
   }
 }
 
+signUp() {
+  console.log('this.state', this.state);
+}
+
     render() {
         return (
             <div className="form-inline">
@@ -18,15 +22,18 @@ constructor(props) {
             className="form-control"
             type="text"
             placeholder="email"
+            onChange={event => this.setState({ email: event.target.value })}
             />
             <input
             className="form-control"
             type="password"
             placeholder="password"
+            onChange={event => this.setState({ password: event.target.value })}
             />
             <button
             className="btn btn-primary"
             type="button"
+            onClick={() => this.signUp()}
             >
             Sign Up
             </button>
