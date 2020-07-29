b7b546279bae991cc900ef4fe05c10e8bd193cf0
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-29 13:51:23 -0700

    Commiting changes

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+    render() {
+        return (
+            <div>SigIn</div>
+        )
+    }
+}
+
+export default SignIn;
