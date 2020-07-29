448b9049acbc0a5d8a231c1c37d1a2627ce904d6
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-29 14:20:39 -0700

    Commiting changes

--- src/index.js
+++ src/index.js
@@ -8,5 +8,7 @@ import SignUp from './components/SignUp';
 
 ReactDOM.render(
 <Router path='/' history={browserHistory}>
-
+<Route path='/app' component={App}/>
+<Route path='/sigin' component={SignIn}/>
+<Route path='/signup' component={SignUp}/>
 </Router>, document.getElementById('root'));
