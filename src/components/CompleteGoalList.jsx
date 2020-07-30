db8bebf115f2863ddd5c6e365528a0ec544b2e25
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-30 00:43:18 -0700

    Commiting changes

--- src/components/CompleteGoalList.jsx
+++ src/components/CompleteGoalList.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import { completeGoalRef } from '../firebase';
 
 class CompleteGoalList extends Component{
+ componentDidMount() {
+completeGoalRef.on('value', snap => {
+  let completeGoals = [];
+  snap.forEach(completeGoal => {
+    const { email, title } = completeGoal.val();
+    completeGoal.push({ email, title })
+  })
+  console.log('completeGoals', completeGoals);
+})
+ }
+
   render() {
     return(
        <div>
