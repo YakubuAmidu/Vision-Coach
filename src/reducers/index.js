b6f05b7ddbd99878da31983de149e9750dfc4d00
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-30 01:19:20 -0700

    Commiting changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './reducer-user';
 import goals from './reducer-goals';
+import completeGoals from './reducer_completed_goals';
 
 export default combineReducers({
   user,
-  goals
+  goals,
+  completeGoals
 })
