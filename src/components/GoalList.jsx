f2eb70b93447c8fb939377f72ec038c037baa9c7
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-29 22:11:38 -0700

    Commiting changes

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -4,10 +4,12 @@ import { goalRef } from '../firebase';
 export default class GoalList extends Component {
 componentDidMount() {
   goalRef.on('value', snap => {
+    let goals = [];
     snap.forEach(goal => {
-      let goalObject = goal.val();
-      console.log('goalObject', goalObject);
+      const { email, title } = goal.val();
+      goals.push({ email, title });
     })
+    console.log('goals', goals);
   })
 }
 
