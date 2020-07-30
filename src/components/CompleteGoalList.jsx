9f1a4716de47b98157be499a224bea9e444dc6a9
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-30 01:47:52 -0700

    Commiting changes

--- src/components/CompleteGoalList.jsx
+++ src/components/CompleteGoalList.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setCompleted } from '../actions';
 import { completeGoalRef } from '../firebase';
 
 class CompleteGoalList extends Component{
@@ -9,17 +11,43 @@ completeGoalRef.on('value', snap => {
     const { email, title } = completeGoal.val();
     completeGoal.push({ email, title })
   })
-  console.log('completeGoals', completeGoals);
+  this.props.setCompleted(completeGoals);
 })
+ }
+
+ clearCompleted() {
+   
  }
 
   render() {
     return(
-       <div>
-      Complete Goal List
+      <div>
+       {
+        this.props.completeGoals.map((completeGoal, index) => {
+          const { title, email } = completeGoal;
+          return (
+            <div key={index}>
+              <strong>{title}</strong> completed by <em>{email}</em>
+            </div>
+          )
+        })
+       }
+       <button
+       className="btn btn-primary"
+       onClick={() => this.clearCompleted()}
+       >
+       Clear All
+       </button>
        </div>
     )
   }
 }
 
-export default CompleteGoalList;
+function mapStateToProps(state) {
+  const { completeGoals } = state;
+  return {
+    completeGoals
+  }
+}
+
+export default connect(mapStateToProps, { setCompleted })(CompleteGoalList);
