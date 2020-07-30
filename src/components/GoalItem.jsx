c8bae4988f3e63405debc6b825637eb61a462d08
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-29 23:37:16 -0700

    Commiting changes

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { completeGoalRef } from '../firebase';
 
 class GoalItem extends Component {
+  completeGoal() {
+  const { email } = this.props.user;
+  const { title } = this.props.goal;
+  }
+
   render() {
-    console.log('this.props.goal', this.props.goal);
     const { email, title } = this.props.goal;
     return (
       <div style={{ marginRight: '5px' }}>
       <strong>{title}</strong>
-      <span>submitted by <em>{email}</em></span>
+      <span style={{ marginRight: '5px' }}>submitted by <em>{email}</em></span>
+      <button
+      className="btn btn-sm btn-primary"
+      onClick={() => this.completeGoal()}
+      >
+      Completed
+      </button>
       </div>
     )
   }
 }
 
-export default GoalItem;
+function mapStateToProps(state) {
+const { user } = state;
+return {
+  user
+}
+}
+
+export default connect(mapStateToProps, null)(GoalItem);
--- src/firebase.js
+++ src/firebase.js
@@ -13,3 +13,4 @@ const config =  {
 
 export const firebaseApp = firebase.initializeApp(config);
 export const goalRef = firebase.database().ref('goals');
+export const completeGoalRef = firebase.database().ref('completeGoals');
