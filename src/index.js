253399a9081359deb8d8f2767614478a7c9bd05c
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-29 20:09:38 -0700

    Commiting changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import { firebaseApp } from './firebase';
 
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+
+const store = createStore();
 
 firebaseApp.auth().onAuthStateChanged(user => {
   if (user) {
@@ -18,8 +22,10 @@ firebaseApp.auth().onAuthStateChanged(user => {
 })
 
 ReactDOM.render(
+<Provider store={store}>
 <Router path="/" history={browserHistory}>
 <Route path="/app" component={App}/>
 <Route path="/signin" component={SignIn}/>
 <Route path="/signup" component={SignUp}/>
-</Router>, document.getElementById('root'));
+</Router>
+</Provider>, document.getElementById('root'))
