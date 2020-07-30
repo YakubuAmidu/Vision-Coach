a5629ba1143af123a164bdcd2c5e73742af08e6f
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-30 00:26:27 -0700

    Commiting changes

---
+++ src/components/CompleteGoalList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CompleteGoalList extends Component{
+  render() {
+    return(
+       <div>
+      Complete Goal List
+       </div>
+    )
+  }
+}
+
+export default CompleteGoalList;
