4efb32cc4a2ae0a2637757d40316fb3af851ec9b
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-09 03:23:50 -0700

    Updates

---
+++ precache-manifest.d092bef62af1533d5604cb76d4709f45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a310ec00916ddb2205ab1d87b67deb32",
+    "url": "/Vision-Coach/index.html"
+  },
+  {
+    "revision": "29cf234200f294a849cd",
+    "url": "/Vision-Coach/static/js/2.0d1d53b5.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Vision-Coach/static/js/2.0d1d53b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39cf6bd7b6982dca3050",
+    "url": "/Vision-Coach/static/js/main.70fd0cd4.chunk.js"
+  },
+  {
+    "revision": "4f120227e7e49b988ae7",
+    "url": "/Vision-Coach/static/js/runtime-main.4dd88f13.js"
+  }
+]);
